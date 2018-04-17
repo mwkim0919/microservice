48c248917bf36bf62b03315f4079529b7bf664bc
Minwoo Kim <mwkim0919@gmail.com>
2018-04-16 22:18:38 -0700

    adding service-limiter

--- service-limiter/src/main/java/com/minwoo/servicelimiter/LimitsConfigurationController.java
+++ service-limiter/src/main/java/com/minwoo/servicelimiter/LimitsConfigurationController.java
@@ -1,4 +1,18 @@
 package com.minwoo.servicelimiter;
 
+import com.minwoo.servicelimiter.beans.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
 }
---
+++ service-limiter/src/main/java/com/minwoo/servicelimiter/ServiceLimiterApplication.java
@@ -0,0 +1,12 @@
+package com.minwoo.servicelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLimiterApplication.class, args);
+	}
+}
